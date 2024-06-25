8bac17ca7fa89c15854a961d21037d173559e510
Judith Ukachukwu <ukachukwujudith95@gmail.com>
2024-06-25 05:27:15 +0100

    forofloop

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,6 +1,5 @@
 export default function appendToEachArrayValue(array, appendString) {
   for (let [idx, value] of array.entries()) {
-   // let value = array[idx];
     array[idx] = appendString + value;
   }
 
