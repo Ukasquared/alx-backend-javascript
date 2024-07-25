c9f114684a005c17bb02145f0d40f45d82361363
Judith Ukachukwu <ukachukwujudith95@gmail.com>
2024-07-25 05:48:12 +0100

    promise building

--- 0x01-ES6_promise/9-try.js
+++ 0x01-ES6_promise/9-try.js
@@ -1,9 +1,9 @@
 export default function guardrail(mathFunction) {
   const queue = [];
   try {
-    queue.push(mathFunction(), 'Guardrail was processed' )
-  } catch(error) {
-    queue.push(`Error: ${error.message}`, 'Guardrail was processed' )
+    queue.push(mathFunction(), 'Guardrail was processed');
+  } catch (error) {
+    queue.push(`Error: ${error.message}`, 'Guardrail was processed');
   }
   return queue;
 }
