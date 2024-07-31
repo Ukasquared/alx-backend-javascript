deb6b1706504ce936734afc5efacf286a5fc8ccf
Judith Ukachukwu <ukachukwujudith95@gmail.com>
2024-07-31 06:05:27 +0100

    data manipulation

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,10 +1,9 @@
 export default function cleanSet(set, startString) {
   let newString = [];
   for (const item of set) {
-    if(item.startsWith(startString))  {
+    if (item.startsWith(startString))  {
       newString.push(item.substring(3)); 
     }
   }
-  newString.join('-');
-  return newString;
+  return newString.join('-');
 }
