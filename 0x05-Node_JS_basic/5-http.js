77626201c8b6f78155237154af89bf0d3a4d6706
Judith Ukachukwu <ukachukwujudith95@gmail.com>
2024-09-20 15:32:15 +0100

    send a response to a http request

---
+++ 0x05-Node_JS_basic/5-http.js
@@ -0,0 +1,32 @@
+const csvFile = process.argv[2];
+const http = require('http');
+const countStud = require('./3-read_file_async')
+
+const port = 1245;
+
+const app = http.createServer((req, res) => {
+  // deconstruct the request
+  const {url, method} = req
+  // return plain text
+  res.writeHead(200, {
+    'Content-Type': 'text/plain'
+  })
+  //if url path is / displays ...
+  if (method === 'GET' && url === '/') {
+    res.end('Hello Holberton School!');
+  } else if (method === 'GET' && url === '/students') {
+  // if url path is /students displays the content of csv file
+    countStud(csvFile)
+      .then((result) => {
+          const response = `This is the list of our students\nNumber of students: ${result[0].length}\nNumber of students in CS: ${result[1].length}. List: ${result[1].join(', ')}\nNumber of students in SWE: ${result[2].length}. List: ${result[2].join(', ')}`
+        res.end(response);
+      })
+     .catch((error) => {
+        res.end(error.message)
+     })
+  }
+})
+
+app.listen(port);
+
+module.exports = app;
