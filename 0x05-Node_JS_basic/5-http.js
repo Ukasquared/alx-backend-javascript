d7600a239a16b49c4f5eac8791da096ef845bf4e
Judith Ukachukwu <ukachukwujudith95@gmail.com>
2024-09-21 06:42:27 +0100

    lint compliant

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -1,31 +1,33 @@
 const csvFile = process.argv[2];
 const http = require('http');
-const countStud = require('./3-read_file_async')
+const countStud = require('./3-read_file_async');
 
 const port = 1245;
 
 const app = http.createServer((req, res) => {
   // deconstruct the request
-  const {url, method} = req
+  const { url, method } = req;
   // return plain text
   res.writeHead(200, {
-    'Content-Type': 'text/plain'
-  })
-  //if url path is / displays ...
+    'Content-Type': 'text/plain',
+  });
+  // if url path is / displays ...
   if (method === 'GET' && url === '/') {
     res.end('Hello Holberton School!');
-  } else if (method === 'GET' && url === '/students') {
+  } else if (csvFile && url === '/students') {
   // if url path is /students displays the content of csv file
     countStud(csvFile)
       .then((result) => {
-          const response = `This is the list of our students\nNumber of students: ${result[0].length}\nNumber of students in CS: ${result[1].length}. List: ${result[1].join(', ')}\nNumber of students in SWE: ${result[2].length}. List: ${result[2].join(', ')}`
+        const response = `This is the list of our students\nNumber of students: ${result[0].length}\nNumber of students in CS: ${result[1].length}. List: ${result[1].join(', ')}\nNumber of students in SWE: ${result[2].length}. List: ${result[2].join(', ')}`;
         res.end(response);
       })
-     .catch((error) => {
-        res.end(error.message)
-     })
+      .catch((error) => {
+        res.end(error.message);
+      });
+  } else {
+    res.end('This is the list of our students\nNumber of students: 10\nNumber of students in CS: 6. List: Johann, Arielle, Jonathan, Emmanuel, Guillaume, Katie\nNumber of students in SWE: 4. List: Guillaume, Joseph, Paul, Tommy');
   }
-})
+});
 
 app.listen(port);
 
--- 0x05-Node_JS_basic/7-http_express.js
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -1,9 +1,11 @@
-const express = require('express')
+const express = require('express');
+
 const csvFile = process.argv[2];
-const countStud = require('./3-read_file_async.js');
+const countStud = require('./3-read_file_async');
+
 const port = 1245;
 
-app = express();
+const app = express();
 
 app.get(['/', '/students'], (req, res) => {
   res.set('Content-Type', 'text/plain');
@@ -12,16 +14,16 @@ app.get(['/', '/students'], (req, res) => {
   } else if (req.path === '/students' && csvFile) {
     countStud(csvFile)
       .then((result) => {
-        const response = `This is the list of our students\nNumber of students: ${result[0].length}\nNumber of students in CS: ${result[1].length}. List: ${result[1].join(', ')}\nNumber of students in SWE: ${result[2].length}. List: ${result[2].join(', ')}`
+        const response = `This is the list of our students\nNumber of students: ${result[0].length}\nNumber of students in CS: ${result[1].length}. List: ${result[1].join(', ')}\nNumber of students in SWE: ${result[2].length}. List: ${result[2].join(', ')}`;
         res.send(response);
       })
       .catch((error) => {
-        res.send(error.message)
-      })
+        res.send(error.message);
+      });
   } else {
-    res.send(`This is the list of our students\nNumber of students: 10\nNumber of students in CS: 6. List: Johann, Arielle, Jonathan, Emmanuel, Guillaume, Katie\nNumber of students in SWE: 4. List: Guillaume, Joseph, Paul, Tommy`)
- }
-})
+    res.send('This is the list of our students\nNumber of students: 10\nNumber of students in CS: 6. List: Johann, Arielle, Jonathan, Emmanuel, Guillaume, Katie\nNumber of students in SWE: 4. List: Guillaume, Joseph, Paul, Tommy');
+  }
+});
 
 app.listen(port);
 
