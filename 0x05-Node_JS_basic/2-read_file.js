ad61821abfd21be3c01d4e8d72049f72fcee2e46
Judith Ukachukwu <ukachukwujudith95@gmail.com>
2024-09-21 06:24:19 +0100

    lint correction

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,29 +1,29 @@
-fs = require('fs')
+const fs = require('fs');
 
-const countStudents = function(path) {
-  fs.readFile(path, 'utf-8', function (err, data) {
+const countStudents = (path) => {
+  fs.readFile(path, 'utf-8', (err, data) => {
     if (err) {
       throw new Error('Cannot load the database');
     }
     const myArray = data.split('\n');
-    const filterArray = myArray.filter(item => item !== "")
-    filterArray.shift()
-    filterArray.forEach(function(value, index, arr) {
-      arr[index] = value.split(',');
-    })
-    const studentCs = []
-    const studentSWE = []
-    filterArray.forEach(function(value, index) {
+    const filterArray = myArray.filter((item) => item !== '');
+    filterArray.shift();
+    filterArray.forEach((value, index) => {
+      filterArray[index] = value.split(',');
+    });
+    const studentCs = [];
+    const studentSWE = [];
+    filterArray.forEach((value) => {
       if (value.includes('CS')) {
         studentCs.push(value.shift());
       } else if (value.includes('SWE')) {
         studentSWE.push(value.shift());
       }
-    })
+    });
     console.log(`Number of students: ${filterArray.length}`);
-    console.log(`Number of students in CS: ${studentCs.length}. List: ${studentCs.join(', ')}`)
-    console.log(`Number of students in SWE: ${studentSWE.length}. List: ${studentSWE.join(', ')}`)
-  })
-}
+    console.log(`Number of students in CS: ${studentCs.length}. List: ${studentCs.join(', ')}`);
+    console.log(`Number of students in SWE: ${studentSWE.length}. List: ${studentSWE.join(', ')}`);
+  });
+};
 
 module.exports = countStudents;
--- 0x05-Node_JS_basic/3-read_file_async.js
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -1,32 +1,32 @@
-fs = require('fs')
+const fs = require('fs');
 
-const countStudents = function(path) {
-  return new Promise(function(resolve, reject) {
-    fs.readFile(path, 'utf-8', function (err, data) {
+const countStudents = function read(path) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(path, 'utf-8', (err, data) => {
       if (err) {
-        return reject(new Error("Cannot load the database"));
+        reject(new Error('Cannot load the database'));
+        return;
       }
       const myArray = data.split('\n');
-      const filterArray = myArray.filter(item => item !== "")
+      const filterArray = myArray.filter((item) => item !== '');
       filterArray.shift();
-      filterArray.forEach(function(value, index, arr) {
-        arr[index] = value.split(',');
-      })
+      filterArray.forEach((value, index) => {
+        filterArray[index] = value.split(',');
+      });
       const studentCs = [];
       const studentSWE = [];
-      filterArray.forEach(function(value, index) {
+      filterArray.forEach((value) => {
         if (value.includes('CS')) {
           studentCs.push(value.shift());
         } else if (value.includes('SWE')) {
           studentSWE.push(value.shift());
         }
-      })
+      });
       console.log(`Number of students: ${filterArray.length}`);
-      console.log(`Number of students in CS: ${studentCs.length}. List: ${studentCs.join(', ')}`)
-      console.log(`Number of students in SWE: ${studentSWE.length}. List: ${studentSWE.join(', ')}`)      
+      console.log(`Number of students in CS: ${studentCs.length}. List: ${studentCs.join(', ')}`);
+      console.log(`Number of students in SWE: ${studentSWE.length}. List: ${studentSWE.join(', ')}`);
       resolve([filterArray, studentCs, studentSWE]);
     });
-  })
-}
-
+  });
+};
 module.exports = countStudents;
